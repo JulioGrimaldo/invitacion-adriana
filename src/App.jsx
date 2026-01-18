ed75bfed59560eadfdbefc740458639d76bdb1ad
Julio Grimaldo <juliogrimaldo1@gmail.com>
2026-01-18 09:13:11 -0600

    Se corrige el tema de la hora en iphone

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { useEffect } from "react"
 
 function App() {
   const [abierto, setAbierto] = useState(false)
-  const fechaEvento = new Date("2026-02-29T19:00:00");
+  const fechaEvento = new Date("2026-02-29T19:00:00-06:00");
+
   const mensajeWhats = encodeURIComponent(
     "Hola Adriana😊\nConfirmo mi asistencia a tu cumpleaños🎉"
   );
